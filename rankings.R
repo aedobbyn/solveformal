8bce384c2dbaa66f636430e7194c34e759272005
Amanda Dobbyn <amanda.e.dobbyn@gmail.com>
2022-10-21 00:21:10 -0400

    printin

--- rankings.R
+++ rankings.R
@@ -4,44 +4,44 @@ library(magrittr)
 
 url <- "https://docs.google.com/spreadsheets/d/1u-J0lPO-6Iz63dGgtguZ1snat8RANoyqiXVt9C9CnVQ/edit#gid=1293285841"
 
-ppl <- 
+ppl <-
   googlesheets4::read_sheet(url, sheet = 2)
 
-ppl <- 
+ppl <-
   c(
-"Aki",
-"CC",
-"Dobby",
-"HB",
-"Iris",
-"Jane",
-"Joyce",
-"Julie",
-"Kayla",
-"KSpa",
-"Kyla",
-"LWoods",
-"Marié",
-"Nancy",
-"Pepi",
-"Sophia",
-"Squid",
-"Wei"
-)
+    "Aki",
+    "CC",
+    "Dobby",
+    "HB",
+    "Iris",
+    "Jane",
+    "Joyce",
+    "Julie",
+    "Kayla",
+    "KSpa",
+    "Kyla",
+    "LWoods",
+    "Marié",
+    "Nancy",
+    "Pepi",
+    "Sophia",
+    "Squid",
+    "Wei"
+  )
 
 
 build_rankings <- function(ppl) {
   out <- tibble()
-  
+
   for (p in ppl) {
-    this <- 
+    this <-
       tibble(
         proposer = p,
         proposee = ppl[-which(ppl == p)],
         ranking = sample(length(ppl) - 1, length(ppl) - 1)
       )
-      
-    out %<>% 
+
+    out %<>%
       bind_rows(
         this
       )
@@ -49,69 +49,6 @@ build_rankings <- function(ppl) {
   out
 }
 
-rankings <- 
-  build_rankings(ppl) %>% 
+rankings <-
+  build_rankings(ppl) %>%
   arrange(proposer, ranking)
-
-
-
-
-
-
-
-rankings <- 
-  # tibble(
-  #   proposer = ppl,
-  #   proposee = ppl
-  # ) %>% 
-  tidyr::expand_grid(
-    proposer = ppl,
-    proposee = ppl
-    # ranking = 1:length(ppl)
-  ) %>% 
-  filter(
-    !proposer == proposee
-  ) %>% 
-  group_by(proposer) %>% 
-  dplyr::sample_n(length(ppl))
-  mutate(
-    ranking = runif(length(ppl), 1, length(ppl))
-  )
-
-  
-  
-  
-rankings <- 
-tibble(
-  proposer = ppl,
-  proposee = ppl
-) %>% 
-  mutate(
-    proposee = list(sample(ppl, length(ppl)))
-  ) %>%
-  tidyr::unnest() %>%
-  filter(
-    !proposer == proposee
-  ) %>% 
-  mutate(
-    ranking = rep(1:(length(ppl)), (length(ppl) - 1))
-  ) %>% 
-  arrange(proposer, ranking) %>% 
-  print(n=300)
-  
-  
-  
-  
-  # tidyr::nest(proposer) %>% 
-  rowwise() %>% 
-  mutate(
-    ranking = list(sample(length(ppl) - 1, length(ppl) - 1))
-  ) %>% 
-  ungroup() %>% 
-  tidyr::unnest() %>% 
-  select(proposer, proposee, ranking) %>% 
-  arrange(proposer, ranking) %>% 
-  print(n=300)
-
-
-
--- solve.R
+++ solve.R
@@ -1,93 +1,121 @@
 
-proposee_rankings <- 
-  rankings %>% 
+proposee_rankings <-
+  rankings %>%
   rename(
     proposer = proposee,
     proposee = proposer,
     proposee_ranking = ranking
   )
-  
+
 proposer_propose <- function(tbl) {
-  tbl %>% 
-    group_by(proposer) %>% 
-    arrange(ranking) %>% 
-    slice(1) %>% 
-    ungroup() %>% 
+  tbl %>%
+    group_by(proposer) %>%
+    arrange(ranking) %>%
+    slice(1) %>%
+    ungroup() %>%
     rename(
       proposer_ranking = ranking
     )
 }
 
 proposee_accept_or_reject <- function(tbl) {
-  tbl %<>% 
-    count(proposee)
-  
-  one_proposal <- 
-    tbl %>% 
+  tbl %<>%
+    add_count(proposee)
+
+  one_proposal <-
+    tbl %>%
     filter(n == 1)
-  
-  multiple_proposals <- 
-    tbl %>% 
-    filter(n > 1) 
-  
-  no_proposal <- 
+
+  multiple_proposals <-
+    tbl %>%
+    filter(n > 1)
+
+  no_proposal <-
     anti_join(
-      rankings %>% 
-        distinct(proposer) %>% 
+      rankings %>%
+        distinct(proposer) %>%
         rename(
           proposee = proposer
         ),
       bind_rows(
-        one_matches,
-        multiples
+        one_proposal,
+        multiple_proposals
       )
-    ) %>% 
+    ) %>%
     select(proposee)
-  
-  multiples_weeded <- 
-    multiple_proposals %>% 
-    inner_join(proposee_rankings) %>% 
-    group_by(proposee) %>% 
-    arrange(proposee_ranking) %>% 
-    slice(1) %>% 
-    ungroup() 
-  
+
+  multiples_weeded <-
+    multiple_proposals %>%
+    inner_join(proposee_rankings) %>%
+    group_by(proposee) %>%
+    arrange(proposee_ranking) %>%
+    slice(1) %>%
+    ungroup()
+
   bind_rows(
     one_proposal,
     multiples_weeded,
     no_proposal
-  )
+  ) %>%
+    select(-proposee_ranking, -n) %>%
+    inner_join(proposee_rankings)
 }
 
-solve <- function(tbl) {
-  n_rounds <- 
-    tbl %>% 
-    distinct(proposer) %>% 
+solve <- function() {
+  n_rounds <-
+    rankings %>%
+    distinct(proposer) %>%
     nrow()
-  
+
   message("Starting round 1")
-  
+
   # First round
-  slate <- 
-    tbl %>% 
-    proposer_propose() %>% 
+  slate <-
+    rankings %>%
+    proposer_propose() %>%
     proposee_accept_or_reject()
-  
+
+  print(slate)
+
+  avg_rankings <-
+    slate %>%
+    summarise(
+      mean_proposer_rank = mean(proposer_ranking),
+      mean_proposee_rank = mean(proposee_ranking)
+    )
+
+  print(avg_rankings)
+
+  proposal_log <- slate
+
   for (r in 2:n_rounds) {
     message(glue::glue("On round {r} of {n_rounds}"))
-    
+
     # Take out people we've already proposed to
-    slate <- 
-      tbl %>% 
-      anti_join(slate) %>% 
-      proposer_propose() %>% 
+    slate <-
+      rankings %>%
+      anti_join(proposal_log) %>%
+      proposer_propose() %>%
       proposee_accept_or_reject()
+
+    print(slate)
+
+    avg_rankings <-
+      slate %>%
+      summarise(
+        mean_proposer_rank = mean(proposer_ranking),
+        mean_proposee_rank = mean(proposee_ranking)
+      )
+
+    print(avg_rankings)
+
+    proposal_log %<>%
+      bind_rows(slate)
   }
-  
+
   slate
 }
 
-solve(rankings)
+solve()
 
 debugonce(solve)
-
